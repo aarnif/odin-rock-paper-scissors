d7bc7a93f452e9190a5624ffd234a0a3ea9f8f22
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-02-22 15:31:40 +0200

    Create basic game ui

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 const getComputerChoice = () => {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
@@ -32,39 +35,61 @@ const playRound = (playerSelection, computerSelection) => {
   return result;
 };
 
+const checkIfGameOver = (playerScore, computerScore) => {
+  return playerScore === 5 || computerScore === 5;
+};
+
 const checkWinner = (playerScore, computerScore) => {
-  if (playerScore > computerScore) {
+  if (playerScore >= 5) {
     return "Congratulations, you won the game! You've triumphed over the opponent with a higher score.";
-  } else if (playerScore < computerScore) {
-    return "Unfortunately, you lost the game. The computer emerged victorious with a higher score.";
   } else {
-    return "The game ends in a draw";
+    return "Unfortunately, you lost the game. The computer emerged victorious with a higher score.";
   }
 };
 
-const playGame = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i < 5; ++i) {
-    const playersChoice = prompt("Choose rock, paper, scissors").toLowerCase();
-    const computerChoice = getComputerChoice();
-    const result = playRound(playersChoice, computerChoice);
-
-    if (result === "Win") {
-      console.log(`You Won! ${playersChoice} beats ${computerChoice}`);
-      playerScore += 1;
-    } else if (result === "Loss") {
-      console.log(`You Lose! ${computerChoice} beats ${playersChoice}`);
-      computerScore += 1;
-    } else {
-      console.log("It's a Tie!");
-    }
-
-    console.log(`Result: Player ${playerScore} Computer ${computerScore}`);
-  }
-
-  const gameResult = checkWinner(playerScore, computerScore);
-  console.log(gameResult);
+const resetScore = () => {
+  playerScore = 0;
+  computerScore = 0;
 };
 
-playGame();
+const displayScore = () => {
+  const scoreBoard = document.getElementById("score");
+  scoreBoard.textContent = `${playerScore} - ${computerScore}`;
+};
+
+const displayText = (message) => {
+  const displayText = document.getElementById("game-text");
+  displayText.textContent = message;
+};
+
+const gameLoop = (playersChoice) => {
+  const computerChoice = getComputerChoice();
+  const result = playRound(playersChoice, computerChoice);
+
+  if (result === "Win") {
+    displayText(`You Won! ${playersChoice} beats ${computerChoice}`);
+    playerScore += 1;
+  } else if (result === "Loss") {
+    displayText(`You Lose! ${computerChoice} beats ${playersChoice}`);
+    computerScore += 1;
+  } else {
+    displayText("It's a Tie!");
+  }
+
+  displayScore();
+
+  if (checkIfGameOver(playerScore, computerScore)) {
+    const gameResult = checkWinner(playerScore);
+    displayText(gameResult);
+    resetScore();
+  }
+};
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const playersChoice = button.id;
+    gameLoop(playersChoice);
+  });
+});
